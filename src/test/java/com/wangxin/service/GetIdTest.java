4aafa47643398aea9594944ad7067c6de7bf8fc4
xwang <xwang@wangxin-PC.handpay.com.cn>
2017-12-21 09:31:26 +0800

    更新

--- src/test/java/com/wangxin/service/GetIdTest.java
+++ src/test/java/com/wangxin/service/GetIdTest.java
@@ -1,6 +1,5 @@
 package com.wangxin.service;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.wangxin.Application;
 import com.wangxin.common.id.redis.RedisIdGenerator;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -21,8 +19,9 @@ public class GetIdTest {
     @Test
     public void getId() {
         try {
-            String result = redisIdGenerator.nextUniqueId("idgenerater", "X-Y", 1, 8);// 从redis读取缓存的值
-            System.out.println(result);
+            System.err.println(redisIdGenerator.nextUniqueId("T_NEWS"));// 从redis读取缓存的值
+            System.err.println(redisIdGenerator.nextUniqueId("T_NEWS"));// 从redis读取缓存的值
+            System.err.println(redisIdGenerator.nextUniqueId("T_SYS_USER"));// 从redis读取缓存的值
         } catch (Exception e) {
             e.printStackTrace();
         }
